b3514e1a8b8c20b4e07f2bfa5c2cc1a4df515f4b
gkarumbi <gkarumbi@gmail.com>
2020-06-01 16:47:32 +0300

    Add event listner functions and wrap them with a main function

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const userScore  = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div =document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+// Add event listeners
+
+main();
+function main(){
+
+    rock_div.addEventListener('click',function(){
+        game("rock");
+    });
+
+    paper_div.addEventListener('click',function(){
+        game("paper");
+    });
+
+    scissors_div.addEventListener('click',function(){
+        game("scissors");
+    });
+}
